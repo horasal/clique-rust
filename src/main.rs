2f452103209372a2d8e9833fa3eb4785a6127610
Hongjie Zhai <zhaihj@live.jp>
2015-11-03 11:43:23 +0900

    load graph from file

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,75 @@
-#![feature(negate_unsigned)]
 
 extern crate petgraph;
 
 mod bronkerbosch;
 mod graph_wrapper;
+mod tomita;
 
 use bronkerbosch::BronKerbosch;
+use tomita::Tomita;
 use graph_wrapper::GraphWrapper;
 
 use petgraph::graphmap::{GraphMap,NodeTrait};
 
+use std::io;
+use std::env;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+pub fn loadFromFile(filename: &str) -> GraphMap<i32, ()> {
+    let mut graph = GraphMap::new();
+    let f = match File::open(filename) {
+        Ok(v) => v,
+        Err(e) => return graph
+    };
+    let mut reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        let line = match line {
+            Ok(l) => l,
+            Err(e) => break 
+        };
+
+        if line.starts_with("#") { continue }
+        let v : Vec<&str> = line.split_whitespace().collect();
+        let n0 = v[0].parse::<i32>().unwrap();
+        let n1 = v[1].parse::<i32>().unwrap();
+        graph.add_node(n0); graph.add_node(n1);
+        if(n0 != n1) { graph.add_edge(n0, n1, ()); }
+    }
+    return graph;
+}
+
+fn print_help() {
+    println!("Clique Enumerator. Syntax:\n programname graph_file method \n 
+             mathed can be: \"BK\" or \"TOMITA\".");
+}
+
 fn main() {
-    println!("First graph's maximal cliques are:");
-    let graph1 = graph1();
-    let mut bk1 = BronKerbosch::new(graph1);
-    bk1.compute();
-    for c in bk1.cliques().iter() {
-        println!("{:?}", c);
-    }
-    println!("------");
-
-    println!("Second graph's maximal cliques are:");
-    let graph2 = graph2();
-    let mut bk2 = BronKerbosch::new(graph2);
-    bk2.compute();
-    for c in bk2.cliques().iter() {
-        println!("{:?}", c);
-    }
-    println!("------");
-
-    println!("Third graph's maximal cliques are:");
-    let graph3 = graph3();
-    let mut bk3 = BronKerbosch::new(graph3);
-    bk3.compute();
-    for c in bk3.cliques().iter() {
-        println!("{:?}", c);
-    }
-    println!("------");
-
+    let args : Vec<_> = env::args().collect();
+    let argv: Vec<&str> = args.iter().map(|x| x.as_ref() ).collect();
+    if args.len() > 2 {
+        let graph4 = loadFromFile(argv[1]);
+        match args[2].to_uppercase().as_ref() {
+            "BK" => {
+                println!("B-K-Algorithm: ");
+                let mut bk4 = BronKerbosch::new(graph4);
+                bk4.compute();
+                for c in bk4.cliques().iter() {
+                    println!("{:?}", c);
+                }
+            },
+            "TOMITA" => {
+                println!("Tomita-Algorithm: ");
+                let graph4 = loadFromFile(argv[1]);
+                let mut tomita = Tomita::new(graph4);
+                tomita.compute();
+                for c in tomita.cliques().iter() {
+                    println!("{:?}", c);
+                }
+            }
+            _ => { print_help() }
+        };
+    } else { print_help() }
 }
-
-fn graph1() -> GraphMap<&'static str,()> {
-    //
-    //  A---B---C
-    //
-    let mut graph = GraphMap::new();
-    graph.add_node("A");
-    graph.add_node("B");
-    graph.add_node("C");
-    graph.add_edge("A", "B", ());
-    graph.add_edge("B", "C", ());
-
-    graph
-}
-
-fn graph2() -> GraphMap<&'static str,()> {
-    //    -B-
-    //   / | \
-    //  A  |  D
-    //   \ | /
-    //    -C-
-    let mut graph = GraphMap::new();
-    graph.add_node("A");
-    graph.add_node("B");
-    graph.add_node("C");
-    graph.add_node("D");
-    graph.add_edge("A", "B", ());
-    graph.add_edge("B", "D", ());
-    graph.add_edge("A", "C", ());
-    graph.add_edge("C", "D", ());
-    graph.add_edge("B", "C", ());
-
-    graph
-}
-
-fn graph3() -> GraphMap<&'static str,()> {
-    //    -B---D
-    //   / |\ /|
-    //  A  | X |
-    //   \ |/ \|
-    //    -C---E
-    let mut graph = GraphMap::new();
-    graph.add_node("A");
-    graph.add_node("B");
-    graph.add_node("C");
-    graph.add_node("D");
-    graph.add_node("E");
-    graph.add_edge("A", "B", ());
-    graph.add_edge("A", "C", ());
-    graph.add_edge("B", "C", ());
-    graph.add_edge("B", "D", ());
-    graph.add_edge("B", "E", ());
-    graph.add_edge("C", "D", ());
-    graph.add_edge("C", "E", ());
-    graph.add_edge("D", "E", ());
-
-    graph
-}
-
-
-
